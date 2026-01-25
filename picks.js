f852733456bdb7bc6ace7ed36ced55543974debf
Arnavmishra142 <mishrarnav142@gmail.com>
2026-01-25 11:48:52 +0530

    Create picks.js
    Signed-off-by: Arnavmishra142 <mishrarnav142@gmail.com>

---
+++ picks.js
@@ -0,0 +1,7 @@
+    , // Comma lagana mat bhoolna pichle wale ke baad
+    {
+        type: "book",
+        title: "Psychology of Money",
+        desc: "Paisa kamana aasaan hai, bachana mushkil.",
+        link: "amazon_link_here"
+    }
